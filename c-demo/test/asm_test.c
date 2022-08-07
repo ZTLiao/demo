9cafbc6597a192e0beed9172d8d6e7d19b8a87b3
liaozetao <lzt@gzqianyin.cn>
2022-08-07 14:09:39 +0800

    补充

--- c-demo/test/asm_test.c
+++ c-demo/test/asm_test.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
-#define JUST_MOV(foo) asm volatile("mov %0,%%eax"::"g"(foo))
 
-void fun() {
-	int i, j, k;
-	i = 0, j = 0, k = 0;
-	asm volatile("mov %%eax,%0\n\tpush %%ebx\n\tpop %1\n\tmov %1,%2":"+a"(i), "=b"(j), "=c"(k));
-	printf("i = %d, j = %d, k = %d\n", i, j, k);
+void func(int argv, char* argc[]) {
+	int* p = (int*) argc;
+	*p = 9999;
+	asm volatile("":::"memory");
+	if (*p == 9999) {
+		printf("9999");
+		return;
+	}
+	printf("8888");
 }
 
-void fun1() {
-	int i, j, k;
-	i = 0, j = 0, k = 0;
-	asm volatile("mov %0,%1":"=b"(j):"a"(1));
-	printf("i = %d, j = %d, k = %d\n", i, j, k);
+void func01() {
+	int cr0 = 5;
+	asm volatile("mov %%cr0,%0":"=a"(cr0));
 }
 
-void fun2() {
-	char str[32] = "fun2...";
-	int i = 0;
-	printf("before str = %s\n", str);
-	asm volatile("mov %0,%1":"=r"(i):"m"(str));
-	printf("after str = %s, i = %x\n", str, i);
+void func02() {
+	int cr0 = 5;
+	asm volatile("mov %%cr0,%0":"+a"(cr0));
 }
 
-void fun3() {
+void func03() {
+	int cr0 = 5;
+	asm volatile("mov %0,%%cr0"::"a"(cr0));
+}
+
+void func04(int argv, char* argc[]) {
+	char* sh = (char*) &argc;
+	asm volatile("lidt %0"::"m"(sh));
+}
+
+void func05(int argv, char* argc[]) {
+	char* sh = (char*) &argc;
+	asm volatile("lidt %0":"=m"(sh));
+}
+
+#define JUST_MOV(foo) asm volatile("mov %0, %%eax"::"g"(foo))
+
+void func06() {
 	JUST_MOV(100);
-	int i = 100;
-	JUST_MOV(i);
-}
-
-void fun4() {
-	int __out = 0;
-	int __in1 = 1;
-	int __int2 = 2;
-	asm volatile("pop %0\n\tmov %1 %%esi\n\tmov %2,%%edi\n\t" : "=a"(__out): "r"(__in1), "r"(__int2));
+	int foo = 100;
+	JUST_MOV(foo);
 }
 
 int main(int argv, char* argc[]) {
-	//fun();
-	//fun1();
-	//fun2();
-	//fun3();
-	fun4();
+	func(argv, argc);
+	func01();
+	func02();
+	func03();
+	func04(argv, argc);
+	func05(argv, argc);
+	func06();
 	return 0;
 }
