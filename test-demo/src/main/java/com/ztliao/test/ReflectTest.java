0012a45cc9427dfd3399b30d1b337e29034c70ac
liaozetao <lzt@gzqianyin.cn>
2022-11-25 13:45:16 +0800

    测试byte-buddy动态创建类型

--- test-demo/src/main/java/com/ztliao/test/ReflectTest.java
+++ test-demo/src/main/java/com/ztliao/test/ReflectTest.java
@@ -1,11 +1,17 @@
 package com.ztliao.test;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.type.TypeDefinition;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author: liaozetao
@@ -14,8 +20,58 @@ import java.util.List;
  */
 public class ReflectTest {
 
-    public static void main(String[] args) throws NoSuchFieldException {
-        for (Method declaredMethod : ReflectTest.class.getDeclaredMethods()) {
+    public static void main(String[] args) throws NoSuchFieldException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        Package aPackage = ReflectTest.class.getPackage();
+        TypeReference<List<String>> typeReference = new TypeReference<List<String>>() {
+        };
+        TypeDescription.Generic build = TypeDescription.Generic.Builder.parameterizedType(List.class, String.class).build();
+        System.out.println(build.asGenericType());
+        TypeDescription.Generic ownerTypeGeneric = Optional.ofNullable(TypeReference.class.getDeclaringClass())
+                .map(TypeDefinition.Sort::describe)
+                .orElse(null);
+        TypeDescription.Generic generic = TypeDescription.Generic.Builder.parameterizedType(
+                        new TypeDescription.ForLoadedType(TypeReference.class),
+                        ownerTypeGeneric,
+                        build)
+                .build();
+
+        Class<?> loaded = new ByteBuddy()
+                .subclass(generic)
+                .name(aPackage.getName() + ".ListTypeReference")
+                .make()
+                .load(Thread.currentThread().getContextClassLoader())
+                .getLoaded();
+        System.out.println(loaded.getName());
+        List<String> list = JSONObject.parseObject("[\"1\"]", (TypeReference) loaded.newInstance());
+        Object obj = JSONObject.parseObject("[\"1\"]", (TypeReference) loaded.newInstance());
+        System.out.println(obj.getClass().getName());
+        for (String s : list) {
+            System.out.println(s);
+        }
+//        new ByteBuddy()
+//                .rebase(TypeReference.class)
+//                .declaredTypes(List.class, Long.class);
+//        Class<?> clazz = new ByteBuddy()
+//                .makeInterface(TypeDescription.Generic.Builder.parameterizedType(TypeReference.class, List.class).build())
+//                .name(TypeReference.class.getName())
+//                .make()
+//                .load(Thread.currentThread().getContextClassLoader())
+//                .getLoaded();
+//        getGeneric(clazz);
+    }
+
+    public static class ListTypeReference extends TypeReference<List<String>> {
+        public ListTypeReference() {
+            super();
+        }
+
+        public ListTypeReference(Type... actualTypeArguments) {
+            super(actualTypeArguments);
+        }
+    }
+
+    public static void getGeneric(Class<?> clazz) {
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
             String methodName = declaredMethod.getName();
             System.out.println(methodName);
             for (Type genericParameterType : declaredMethod.getGenericParameterTypes()) {
@@ -27,9 +83,5 @@ public class ReflectTest {
                 }
             }
         }
-    }
-
-    public String helloWorld(List<Long> list) {
-        return "hello,world!";
     }
 }
