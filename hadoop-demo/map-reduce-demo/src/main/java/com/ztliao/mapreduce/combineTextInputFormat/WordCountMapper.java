f1213a2c61786a4aeb03f47858bcb25b0824373b
liaozetao <1107136310@qq.com>
2023-07-24 09:45:10 +0800

    新增combine的demo

---
+++ hadoop-demo/map-reduce-demo/src/main/java/com/ztliao/mapreduce/combineTextInputFormat/WordCountMapper.java
@@ -0,0 +1,42 @@
+package com.ztliao.mapreduce.combineTextInputFormat;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @author: liaozetao
+ * @date: 2023/7/22 11:06
+ * @description:
+ */
+
+/**
+ * KEYIN：map阶段输入的key的类型，LongWriteable
+ * VALUEIN：map阶段输入value类型，Text
+ * KEYOUT：map阶段输出的key类型，Text
+ * VALUEOUT：map阶段输出的value类型，IntWriteable
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final Text outK = new Text();
+
+    private final IntWritable outV = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        //1.获取一行
+        String line = value.toString();
+        //2.切割
+        String[] words = line.split(" ");
+        //3.循环写出
+        for (String word : words) {
+            //封装out
+            outK.set(word);
+            //写出
+            context.write(outK, outV);
+        }
+    }
+}
